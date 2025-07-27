c36ddcb6fe98e89fdb3776b3a1911605927de1db
Sunit2003-mallick <sunitkumarmallick2003@gmail.com>
2025-07-27 15:19:49 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(4 + 4)  // we are using nodejs, not browser
+
+// also maintain code readability
+console.log(3 + 3)
+console.log("Sunit")
+
+let name = "Sunit"
+let age = 22
+let isLoggedin = false
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => undefined
+// symbol => unique
+
+
+//object
+
+
+console.log(typeof "Sunit")
+console.log(typeof undefined)
+console.log(typeof null)
