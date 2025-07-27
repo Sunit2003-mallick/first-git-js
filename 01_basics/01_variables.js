3b39e97cdb31d7bcd2736b9344d210c3b3956909
Sunit2003-mallick <sunitkumarmallick2003@gmail.com>
2025-07-27 15:00:00 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "sun@google.com"
+var accountPassword = "1234"
+accountCity = "BBSR"
+let accountState
+
+// accountId = 12
+accountEmail = "abab@gm.com"
+accountPassword = "22"
+accountCity = "Chennai"
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
