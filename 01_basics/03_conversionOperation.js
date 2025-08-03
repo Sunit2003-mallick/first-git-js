b20b6ac132ec1fec6bb199ed8191e73e052afa55
Sunit2003-mallick <sunitkumarmallick2003@gmail.com>
2025-08-03 16:15:50 +0000

    modified conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -46,11 +46,48 @@
 // output :- boolean - number - 1
 
 
-let isLoggedIn = "sunit"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = "sunit"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// output :- 1 => true; 0 => false; "" => false; "sunit" => true;
 
-// 1 => true; 0 => false; "" => false; "sunit" => true;
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+//output :- string - 33
+
+
+
+
+// **** operations ****
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+// output :- -3
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+//output :- 4,0,4,1,4,0
+
+
+// let str1 = "hello"
+// let str2 = " sunit"
+// let str3 = str1 + str2
+// console.log(str3);
+// output :- hello sunit
+
+
+
+
+
 
 
 
