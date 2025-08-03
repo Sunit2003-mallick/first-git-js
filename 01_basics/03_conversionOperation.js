980a96e1bdc8dde639c04f902a593e47e851649d
Sunit2003-mallick <sunitkumarmallick2003@gmail.com>
2025-08-03 15:48:40 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,56 @@
+// let score = 33
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// output :- number - number - 33 
+
+
+// let score = "33"
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// output :- string - number - 33
+
+
+// let score = "33ab"
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// output :- string - number - NaN
+
+
+// let score = null
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// output :- object - number - 0
+
+
+// let score = undefined
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// output :- undefined - number - NaN
+
+
+// let score = true
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// output :- boolean - number - 1
+
+
+let isLoggedIn = "sunit"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false; "" => false; "sunit" => true;
+
+
+
